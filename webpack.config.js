f43f01f88ae3eb5f69f9d95d8c46dcad7b5042da
Ed 'seddy' Saunders <saunders.ed@gmail.com>
2016-07-04 16:56:20 +0100

    [+AA] Compile SCSS and naively load it
    Simply having a stylesheet "link" tag seems a bit ham-fisted, but we're
    not sure what the best practice is around this (fingers crossed Victor
    will point us in the right direction...).
    
    Margin 0 around body and headers means we don't get unwanted whitespace
    around the app bar

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const ROOT_PATH = path.resolve(__dirname)
 const APP_PATH = path.resolve(ROOT_PATH, 'app/js')
 const APP_PATH_ENTRY = APP_PATH + '/app.jsx'
 const BUILD_PATH = path.resolve(ROOT_PATH, 'dist')
+const STYLES_PATH = path.resolve(ROOT_PATH, 'app/styles')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const cssExtract = new ExtractTextPlugin('analytics.css')
 
 module.exports = {
   entry: APP_PATH_ENTRY,
@@ -16,6 +20,14 @@ module.exports = {
   module: {
     loaders: [
       { test : /\.jsx?/, include : APP_PATH, loader : 'babel' },
+      {
+        test: /\.scss$/,
+        loader: cssExtract.extract('style-loader', 'css-loader!postcss-loader!sass-loader'),
+        include: STYLES_PATH,
+      },
     ],
   },
+  plugins: [
+    cssExtract
+  ],
 };
