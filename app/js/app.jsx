41f679a36a3b087eed9fc054936efe998e108a75
Ed 'seddy' Saunders <saunders.ed@gmail.com>
2016-07-04 16:40:18 +0100

    [+AA] Create simple Material Design App Bar

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -1,10 +1,18 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
-}
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CPAppBar from './components/cp_app_bar.jsx';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <CPAppBar />
+  </MuiThemeProvider>
+);
 
 render(<App/>, document.getElementById('app'));
---
+++ app/js/components/cp_app_bar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const CPAppBar = () => (
+  <AppBar title="HAIL US" />
+)
+
+export default CPAppBar;
