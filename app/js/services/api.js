1f3cada11ae5c07ef299d19b15871d0aa5e1fdc8
Ionut Alexandru Anca <ionut998@gmail.com>
2016-08-01 18:32:09 +0300

    Fix environment variables add CP_ANALYTICS_BACKEND and make the slashes at the end of url consistent

--- app/js/services/api.js
+++ app/js/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
-import { CP_ANALYTICS_HOST, TABLEAU_GATEWAY_API } from '../constants/app_constants'
+import { CP_ANALYTICS_HOST, TABLEAU_GATEWAY_API, CP_ANALYTICS_BACKEND } from '../constants/app_constants'
 
 export const arrayToParam = (name, values) =>
   values.map((value) => `${name}[]=${value}`).join('&')
 
 const Api = {
-  getTableauToken: () => axios.get(`${CP_ANALYTICS_HOST}/api/v1/tableau/token`),
+  getTableauToken: () => axios.get(`${CP_ANALYTICS_BACKEND}tableau/token`),
 
-  getWorkbooks: (workbookIds) => axios.get(`${TABLEAU_GATEWAY_API}/workbooks?${arrayToParam('ids', workbookIds)}`),
+  getWorkbooks: (workbookIds) => axios.get(`${TABLEAU_GATEWAY_API}workbooks?${arrayToParam('ids', workbookIds)}`),
 }
 
 export default Api
--- envConfig.js
+++ envConfig.js
@@ -1,6 +1,7 @@
 exports.envConfig = JSON.stringify({
   CP_ADMIN_HOST: process.env.CP_ADMIN_HOST,
   CP_ANALYTICS_HOST: process.env.CP_ANALYTICS_HOST,
+  CP_ANALYTICS_BACKEND: process.env.CP_ANALYTICS_BACKEND,
   TABLEAU_GATEWAY_API: process.env.TABLEAU_GATEWAY_API,
   TABLEAU_HOST: process.env.TABLEAU_HOST,
 });
