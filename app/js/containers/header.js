d0200feae9cc4c9be347e8af4c7fe8760a45ad48
Tom Currell <tomdsyne@gmail.com>
2016-08-17 14:48:46 +0100

    Remove line break from header.js

--- app/js/containers/header.js
+++ app/js/containers/header.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux'
 import { toggleLeftHandNav } from '../actions/actions.js'
 import Header from '../components/header/header.jsx'
-
 const mapStateToProps = (state) => {
   // There are no props to map, but we need a function to call connect()!
   return {}
