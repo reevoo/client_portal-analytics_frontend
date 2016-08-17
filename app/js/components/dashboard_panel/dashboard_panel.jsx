41e5e7fcff622ccdc2cbf18d5046f844860d375b
Tom Currell <tomdsyne@gmail.com>
2016-08-17 14:45:36 +0100

    Add userId to UUID param in iframe URL

--- app/js/components/dashboard_panel/dashboard_panel.jsx
+++ app/js/components/dashboard_panel/dashboard_panel.jsx
@@ -2,11 +2,11 @@ import React, { PropTypes } from 'react'
 import { TABLEAU_HOST } from '../../constants/app_constants'
 import './dashboard_panel.scss'
 
-const DashboardPanel = ({leftHandNavVisible, dashboard, token}) => {
-  return token && dashboard
+const DashboardPanel = ({ leftHandNavVisible, dashboard, token, userId }) => {
+  return dashboard && token
       ? <div className={`dashboard-panel ${leftHandNavVisible ? 'collapsed' : 'expanded'}`}>
         <iframe
-          src={`${TABLEAU_HOST}trusted/${token}/views/${dashboard.views[0].replace('sheets/', '')}?:embed=yes&:toolbar=no&:showShareOptions=no&:record_performance=yes`} />
+          src={`${TABLEAU_HOST}trusted/${token}/views/${dashboard.views[0].replace('sheets/', '')}?:embed=yes&:toolbar=no&:showShareOptions=no&:record_performance=yes&UUID=${userId}`} />
       </div>
       : null
 }
@@ -15,6 +15,7 @@ DashboardPanel.propTypes = {
   leftHandNavVisible: PropTypes.bool.isRequired,
   dashboard: PropTypes.object,
   token: PropTypes.string,
+  userId: PropTypes.string,
 }
 
 export default DashboardPanel
